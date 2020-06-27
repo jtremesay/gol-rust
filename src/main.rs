97f7ca494a2e474b0310f4d604ea05907cba982c
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2020-06-28 00:32:41 +0200

    can disable the rendering

--- src/main.rs
+++ src/main.rs
@@ -101,6 +101,39 @@ impl World {
     }
 }
 
+enum RenderBackend {
+    None,
+    Piston,
+}
+
+trait Render {
+    fn render(&mut self, world: &World);
+}
+
+struct NoneRenderBackend {}
+
+impl NoneRenderBackend {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Render for NoneRenderBackend {
+    fn render(&mut self, _: &World) {}
+}
+
+struct PistonRenderBackend {}
+
+impl PistonRenderBackend {
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Render for PistonRenderBackend {
+    fn render(&mut self, _: &World) {}
+}
+
 fn usage() {
     println!("Usage: gol [--help] [--width width] [--height height] [--max-steps steps]");
     println!("");
@@ -111,6 +144,9 @@ fn usage() {
     println!("    --density density  Define the initial density of population of the world (default 0.5)");
     println!("    --max-steps steps  The number of steps to run of the simulation (default 0)");
     println!("    --loop steps       Run the simulation for ever (enabled by default)");
+    println!(
+        "    --render backend   The render backend to use (default piston) (available piston none"
+    );
 }
 
 fn main() {
@@ -123,6 +159,7 @@ fn main() {
     let mut run_forever = true;
     let mut display_help = false;
     let mut arg_index = 1;
+    let mut render_backend_type = RenderBackend::Piston;
     while arg_index < args.len() {
         let current_arg = &args[arg_index];
         let next_arg = if arg_index + 1 == args.len() {
@@ -177,6 +214,21 @@ fn main() {
         } else if current_arg == "--loop" {
             max_steps = 0;
             run_forever = true;
+        } else if current_arg == "--render" {
+            if let Some(render) = next_arg {
+                if render == "none" {
+                    render_backend_type = RenderBackend::None;
+                } else if render == "piston" {
+                    render_backend_type = RenderBackend::Piston;
+                } else {
+                    panic!("Unknow value {} for parameter --render", render);
+                }
+
+                // Consume the arg
+                arg_index += 1;
+            } else {
+                panic!("Missing value for parameter --render")
+            }
         } else {
             panic!("Unexpected remaining argument {}", current_arg)
         }
@@ -195,43 +247,78 @@ fn main() {
     let mut world = World::new(world_width, world_height);
     world.populate(world_density);
 
-    // Create the main window
-    let mut window: piston_window::PistonWindow = piston_window::WindowSettings::new(
-        "Hello Piston!",
-        [world_width as u32, world_height as u32],
-    )
-    .exit_on_esc(true)
-    .build()
-    .unwrap();
+    // Create the window if needed
+    let mut window: Option<piston_window::PistonWindow> = match render_backend_type {
+        RenderBackend::Piston => Some(
+            piston_window::WindowSettings::new(
+                "Hello Piston!",
+                [world_width as u32, world_height as u32],
+            )
+            .exit_on_esc(true)
+            .build()
+            .unwrap(),
+        ),
+        _ => None,
+    };
 
     // Main loop
     let mut current_step = 0;
-    while let Some(event) = window.next() {
+    //while let Some(event) = window.next() {
+    loop {
+        println!("running step {}_", current_step);
+        let step_start = std::time::SystemTime::now();
+
         if !run_forever && current_step == max_steps {
             break;
         }
 
         // Update the world
-        world.update();
+        {
+            println!("update world...");
+            let update_start = std::time::SystemTime::now();
+            world.update();
+            let update_end = std::time::SystemTime::now();
+            let update_duration = update_end.duration_since(update_start).unwrap();
+            println!("update done, took {:?}", update_duration);
+        }
 
-        // Do the render
-        window.draw_2d(&event, |context, graphics, _device| {
-            piston_window::clear([1.0; 4], graphics);
+        // Render the world
+        {
+            println!("render world...");
+            let render_start = std::time::SystemTime::now();
+            if let Some(mut window_) = window.as_mut() {
+                if let Some(event) = window_.next() {
+                    window_.draw_2d(&event, |context, graphics, _device| {
+                        piston_window::clear([1.0; 4], graphics);
 
-            for y in 0..world.height {
-                for x in 0..world.width {
-                    let cell_state = world.tiles[y][x];
-                    if cell_state == CellState::Alive {
-                        piston_window::rectangle(
-                            [0.0, 0.0, 0.0, 1.0],
-                            [x as f64, y as f64, 1.0, 1.0],
-                            context.transform,
-                            graphics,
-                        );
-                    }
+                        for y in 0..world.height {
+                            for x in 0..world.width {
+                                let cell_state = world.tiles[y][x];
+                                if cell_state == CellState::Alive {
+                                    piston_window::rectangle(
+                                        [0.0, 0.0, 0.0, 1.0],
+                                        [x as f64, y as f64, 1.0, 1.0],
+                                        context.transform,
+                                        graphics,
+                                    );
+                                }
+                            }
+                        }
+                    });
                 }
             }
-        });
+            let render_end = std::time::SystemTime::now();
+            let render_duration = render_end.duration_since(render_start).unwrap();
+            println!("render done, took {:?}", render_duration);
+        }
+
+        let step_end = std::time::SystemTime::now();
+        let step_duration = step_end.duration_since(step_start).unwrap();
+        println!(
+            "step done, took {:?} ({:.0} FPS)",
+            step_duration,
+            1.0 / step_duration.as_secs_f64()
+        );
 
         current_step += 1;
     }
